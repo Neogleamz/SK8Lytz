ee40f099099f163f4cb640267d498d3228e72163
neogleamz <services@neogleamz.com>
2026-03-20 05:15:18 -0500

    chore: setup EAS build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'mobile-web-demo',
-      script: 'npx',
-      args: 'serve dist -p 8081',
-      interpreter: 'none',
+      script: 'node_modules/serve/build/main.js',
+      args: ['dist', '-p', '8081'],
       env: {
         NODE_ENV: 'production',
       },
