927faa142f18a23ab355d66cc4120f7177a231a1
neogleamz <services@neogleamz.com>
2026-03-23 05:43:21 -0500

    chore: save local changes before wsl build

--- android_env.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export ANDROID_HOME=/opt/android-sdk
-export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/34.0.0
